bda510379460c1830502d14b8d4373244714a1a2
Sheikh Atiqa <162320973+Sheikhatiqaofficial@users.noreply.github.com>
2024-03-08 20:59:52 +0500

    Add files via upload

---
+++ CLASS-3(bio)/class3a.ts
@@ -0,0 +1,50 @@
+let myName: string ="ATIQA";
+console.log(myName);
+
+const age: number = 19;
+console.log(age);
+
+let email: string = "ABC1234@gmail.com";
+console.log(email);
+
+const number: number = +9231522428655875;
+console.log (number);
+
+let isstudent: boolean = true;
+console.log(isstudent);
+
+let isemployed: boolean = false;
+console.log(isemployed);
+
+let address: any = "ABC colony,quater#5,Karaci,Pakistan";
+console.log(address);
+
+let city: string ="Karachi";
+console.log(city);
+
+let country: string ="Pakistan";
+console.log(country);
+
+let skills: string ="canva, fiver, content writing, resumes making, amazon virtual assistant...i.e";
+console.log(skills);
+
+let ismarried: boolean =false;
+console.log(ismarried);
+
+let height: number =5.7;
+console.log(height);
+
+const matriculation: string ="Computer science";
+console.log(matriculation);
+
+const intermediate: string ="Commerce";
+console.log(intermediate);
+
+let weight: number = 45;
+console.log(weight);
+
+let isgraduate: boolean =false;
+console.log(isgraduate);
+
+const matriculationYear: number = 2020;
+console.log(matriculationYear);
